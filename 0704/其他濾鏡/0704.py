ab439672caceb7333c5b819b90ffa9e027f2d554
yayun502 <91117215+yayun502@users.noreply.github.com>
2022-07-03 22:00:21 +0800

    Update 0704.py

--- 0704/其他濾鏡/0704.py
+++ 0704/其他濾鏡/0704.py
@@ -5,22 +5,22 @@ import numpy as np
 cap = cv2.VideoCapture(0)
 
 
-# 获取图像中的人脸关键点
-# 输入
-# img ： 图像
-# det_face ： 人脸检测器
-# det_landmarks ： 人脸关键点检测器
+# 得到圖片中的人臉關鍵點
+# 輸入參數
+# img ： 圖片
+# det_face ： 人臉檢測器
+# det_landmarks ： 人臉關鍵點檢測器
 def get_landmarks_points(img, det_face, det_landmarks):
-    # 转换为灰度
+    # 轉換成灰階圖片
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # 检测人脸区域
+    # 檢測人臉區域
     face_rects = det_face(gray, 0)
 
-    # 获取68个关键点
+    # 得到dlib的68個關鍵點
     landmarks = det_landmarks(gray, face_rects[0])
 
-    # 获取关键点的坐标
+    # 得到關鍵點的坐標
     landmarks_points = []
     parts = landmarks.parts()
     for part in parts:
@@ -28,25 +28,25 @@ def get_landmarks_points(img, det_face, det_landmarks):
     return landmarks_points
 
 
-# 获取内解三角形的顶点list
-# landmarks_points ： 68个关键点的坐标
+# 得到小三角形們的頂點list
+# landmarks_points ： 68個關鍵點的坐標
 def get_tri_pt_index_list(landmarks_points):
     points = np.array(landmarks_points, np.int32)
 
-    # 得到人脸区域的凸包
+    # 得到人臉區域的convex
     convexhull = cv2.convexHull(points)
 
-    # 获取凸包的外截矩形
+    # 得到該convex的外接矩形
     rect = cv2.boundingRect(convexhull)
 
-    # 定义subdiv 用来进行三角剖分
+    # 利用subdiv進行三角剖分
     subdiv = cv2.Subdiv2D(rect)
     subdiv.insert(landmarks_points)
     triangles = subdiv.getTriangleList()
 
     triangles = np.array(triangles, dtype=np.int32)
 
-    # 获取每个三角形的坐标位置
+    # 得到每個三角形的坐標位置
     list_index_tris = []
     for t in triangles:
         pt1 = (t[0], t[1])
@@ -63,7 +63,7 @@ def get_tri_pt_index_list(landmarks_points):
 
 
 def get_one_rect_from_tri(img, tri, landmarks):
-    # 获取三个顶点的坐标
+    # 得到三個頂點的坐標'
     pt1 = landmarks[tri[0]]
     pt2 = landmarks[tri[1]]
     pt3 = landmarks[tri[2]]
@@ -73,10 +73,10 @@ def get_one_rect_from_tri(img, tri, landmarks):
     crop_rect = cv2.boundingRect(points)
     (x, y, w, h) = crop_rect
 
-    # 计算三个顶点在外接矩形上的坐标
+    # 計算三個頂點在外接矩形上的坐標((0,0)為起始點)
     points_in_rect = points - np.array([(x, y)])
 
-    # 进行图像截取
+    # 擷取該圖片
     crop_img = img[y:y + h, x:x + w]
 
     return crop_img, crop_rect, points_in_rect
@@ -86,28 +86,28 @@ def get_face_cover(img_src, img_dst, landmarks_src, landmarks_dst, list_index_tr
     img_cover = np.zeros_like(img_dst, np.uint8)
 
     for tri in list_index_tris:
-        # 源图片的一个三角上截取一个矩形
+        # source image的三角形上截取一個矩形(才能做後續轉換，因為三角形不行相互轉換)
         crop_img_src, crop_rect_src, points_in_rect_src = get_one_rect_from_tri(img_src, tri, landmarks_src)
 
-        # 从目标图片的相同位置的三角上也截取一个矩形
+        # 從destination image的相同位置上也截取一個矩形
         crop_img_dst, crop_rect_dst, points_in_rect_dst = get_one_rect_from_tri(img_dst, tri, landmarks_dst)
 
-        # 计算变换矩阵
+        # 計算tranform matrix
         pts_src = np.float32(points_in_rect_src)
         pts_dst = np.float32(points_in_rect_dst)
         M = cv2.getAffineTransform(pts_src, pts_dst)
 
-        # 实现源 到目标的转换
+        # 進行轉換(source to destination)
         (x, y, w, h) = crop_rect_dst
         warped_src = cv2.warpAffine(crop_img_src, M, (w, h))
 
-        # 由于只替换目标图像中三角区域内的部分
-        # 因此需要做一个mask,对三角区域内填充255，其他部分填充0
+        # 因為只需替換圖片中三角形區域
+        # 因此需要建立一個mask, 對三角形區域裡填充值255，其他部分填充值為0
         mask_dst = np.zeros((h, w), np.uint8)
         cv2.fillConvexPoly(mask_dst, points_in_rect_dst, 255)
         warped_tri = cv2.bitwise_and(warped_src, warped_src, mask=mask_dst)
 
-        # 直接叠加三角连接处会重复相加，因此在叠加时，只对区域内非零的部分进行叠加
+        # 直接疊加三角形，則連接處會重複相加，所以在疊加時，只針對區域內值非零的部分進行疊加
         # img_mask[y:y+h,x:x+w] = img_mask[y:y+h,x:x+w] + warped_tri
         img_area = img_cover[y:y + h, x:x + w].copy()
         img_area_gray = cv2.cvtColor(img_area, cv2.COLOR_BGR2GRAY)
